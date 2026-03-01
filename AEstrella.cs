89a74cea673385f59f03a941b8802f68e3b891ad
jesus <jgarridocruz5@gmail.com>
2026-03-01 12:16:58 -0600

    Se configuro el primer desvio ante un obstaculo al recorrer y

--- AEstrella.cs
+++ AEstrella.cs
@@ -8,11 +8,63 @@ namespace A_EstrellaV2
 {
     internal class AEstrella
     {
+        public class PointXY
+        {
+            public int x { get; set; }
+            public int y { get; set; }
+
+            public PointXY(int x, int y)
+            {
+                this.x = x;
+                this.y = y;
+            }
+        }
         public static void ForAtoM()
         {
+            PointXY actual = new PointXY(0, 0);
+            PointXY meta = new PointXY(0, 4);
+            PointXY[] obstaculos = [
+                new PointXY(0, 2),
+                new PointXY(4, 2)
+                ];
+            System.Diagnostics.Debug.WriteLine("\n---------------------------------------------------------\n");
+
+            int x = actual.x;
+            int y = actual.y;
+            while (y < meta.y)
+            {
+                System.Diagnostics.Debug.WriteLine("1.- " + x + " " + y + "\n");
+                if (x == obstaculos[0].x && y + 1 == obstaculos[0].y)
+                {
+                    x++;
+                }
+                else
+                {
+                    y++;
+                }
+            }
+
+            if (x < meta.x)
+            {// Lo mismo de arriba pero con X
+                while (x <= meta.x)
+                {
+                    System.Diagnostics.Debug.WriteLine("2.- " + x + " " + y + "\n");
+                    x++;
+                }
+            }
+            else
+            {
+                while (x >= meta.x)
+                {
+                    System.Diagnostics.Debug.WriteLine("2.- " + x + " " + y + "\n");
+                    x--;
+                }
+            }
+
+            /*
             int[] actual = [0, 0];
             int[] meta = [4, 4];
-            string res = "\n---------------------------------------------------------";
+            string res = "\n---------------------------------------------------------\n";
             // Primero nos vamos en Y
             int y = actual[1];
             if (actual[1] < meta[1])
@@ -51,6 +103,7 @@ namespace A_EstrellaV2
                 }
             }
             System.Diagnostics.Debug.WriteLine(res);
+             */
         }
     }
 }
