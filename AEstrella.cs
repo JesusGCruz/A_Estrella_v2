06eb534ee9092a23e13bea6dcabd8a93a197877f
jesus <jgarridocruz5@gmail.com>
2026-02-28 20:14:30 -0600

    Se divio la UI del algoritmo

---
+++ AEstrella.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A_EstrellaV2
+{
+    internal class AEstrella
+    {
+        public static void ForAtoM()
+        {
+            int[] actual = [0, 0];
+            int[] meta = [4, 4];
+            string res = "";
+            // Primero nos vamos en Y
+            int y = actual[1];
+            if (actual[1] < meta[1])
+            {// Si Yactual es menor que Ymeta; se sigue con normalidad
+                while (y < meta[1])
+                {
+                    res += actual[0] + " " + y + "\n";
+                    y++;
+                }
+            }
+            else
+            {// Si Yactual es mayor que Ymeta; se va pa atrás
+                while (y > meta[1])
+                {
+                    res += actual[0] + " " + y + "\n";
+                    y--;
+                }
+            }
+
+            // Luego nos vamos en X
+            int x = actual[0];
+            if (actual[0] < meta[0])
+            {// Lo mismo de arriba pero con X
+                while (x <= meta[0])
+                {
+                    res += x + " " + meta[1] + "\n";
+                    x++;
+                }
+            }
+            else
+            {
+                while (x >= meta[0])
+                {
+                    res += x + " " + meta[1] + "\n";
+                    x--;
+                }
+            }
+            System.Diagnostics.Debug.WriteLine(res);
+        }
+    }
+}
--- UIAlgoritmo.cs
+++ UIAlgoritmo.cs
@@ -4,62 +4,8 @@ namespace A_EstrellaV2
     {
         public UIAlgoritmo()
         {
+            AEstrella.ForAtoM();
             InitializeComponent();
-            // Coordenadas X, Y iniciales (a - Actual) y finales (m - Meta)
-            int xA, xM, yA, yM;
-            xA = yA = 4;
-            xM = yM = 0;
-
-            lbResponse.Text = ForAtoM([xA, yA], [xM, yM]);
-        }
-        private string ForAtoM(int[] actual, int[] meta)
-        {
-            string res = "";
-            int x = actual[0];
-            int y = actual[1];
-            if (actual[1] < meta[1])
-            {// Si Yactual es menor que Ymeta; se sigue con normalidad
-                while (y < meta[1])
-                {
-                    res += actual[0] + " " + y + "\n";
-                    y++;
-                }
-            }
-            else
-            {// Si Yactual es mayor que Ymeta; se va pa atrás
-                while (y > meta[1])
-                {
-                    res += actual[0] + " " + y + "\n";
-                    y--;
-                }
-            }
-            if (actual[0] < meta[0])
-            {// Lo mismo de arriba pero con X
-                while (x <= meta[0])
-                {
-                    res += x + " " + meta[1] + "\n";
-                    x++;
-                }
-            }
-            else
-            {
-                while (x >= meta[0])
-                {
-                    res += x + " " + meta[1] + "\n";
-                    x--;
-                }
-            }
-                /*
-                for (int y = actual[1]; y < meta[1]; y++)
-                {
-                    res += actual[0] + " " + y + "\n";
-                }
-                for (int x = actual[0]; x <= meta[0]; x++)
-                {
-                    res += x + " " + meta[1] + "\n";
-                }
-                 */
-                return res;
         }
     }
 }
