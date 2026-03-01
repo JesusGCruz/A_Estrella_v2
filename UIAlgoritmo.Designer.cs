782da2477b72c12379f21a1b5fb447ec8b71c4f0
Lupita <luher2912@gmail.com>
2026-03-01 17:43:56 -0600

    Realice el UI

--- UIAlgoritmo.Designer.cs
+++ UIAlgoritmo.Designer.cs
@@ -28,32 +28,348 @@
         /// </summary>
         private void InitializeComponent()
         {
-            lbResponse = new Label();
+            flowLayoutPanel1 = new FlowLayoutPanel();
+            btn1 = new Button();
+            btn2 = new Button();
+            btn3 = new Button();
+            btn4 = new Button();
+            btn5 = new Button();
+            btn6 = new Button();
+            btn7 = new Button();
+            btn8 = new Button();
+            btn9 = new Button();
+            btn10 = new Button();
+            btn11 = new Button();
+            btn12 = new Button();
+            btn13 = new Button();
+            btn14 = new Button();
+            btn15 = new Button();
+            btn16 = new Button();
+            btn17 = new Button();
+            btn18 = new Button();
+            btn19 = new Button();
+            btn20 = new Button();
+            btn21 = new Button();
+            btn22 = new Button();
+            btn23 = new Button();
+            btn24 = new Button();
+            btn25 = new Button();
+            btnPlay = new Button();
+            btnReiniciar = new Button();
+            panel1 = new Panel();
+            flowLayoutPanel1.SuspendLayout();
             SuspendLayout();
             // 
-            // lbResponse
+            // flowLayoutPanel1
             // 
-            lbResponse.AutoSize = true;
-            lbResponse.Location = new Point(268, 56);
-            lbResponse.Name = "lbResponse";
-            lbResponse.Size = new Size(38, 15);
-            lbResponse.TabIndex = 0;
-            lbResponse.Text = "label1";
+            flowLayoutPanel1.BackColor = Color.FromArgb(140, 55, 38);
+            flowLayoutPanel1.Controls.Add(btn1);
+            flowLayoutPanel1.Controls.Add(btn2);
+            flowLayoutPanel1.Controls.Add(btn3);
+            flowLayoutPanel1.Controls.Add(btn4);
+            flowLayoutPanel1.Controls.Add(btn5);
+            flowLayoutPanel1.Controls.Add(btn6);
+            flowLayoutPanel1.Controls.Add(btn7);
+            flowLayoutPanel1.Controls.Add(btn8);
+            flowLayoutPanel1.Controls.Add(btn9);
+            flowLayoutPanel1.Controls.Add(btn10);
+            flowLayoutPanel1.Controls.Add(btn11);
+            flowLayoutPanel1.Controls.Add(btn12);
+            flowLayoutPanel1.Controls.Add(btn13);
+            flowLayoutPanel1.Controls.Add(btn14);
+            flowLayoutPanel1.Controls.Add(btn15);
+            flowLayoutPanel1.Controls.Add(btn16);
+            flowLayoutPanel1.Controls.Add(btn17);
+            flowLayoutPanel1.Controls.Add(btn18);
+            flowLayoutPanel1.Controls.Add(btn19);
+            flowLayoutPanel1.Controls.Add(btn20);
+            flowLayoutPanel1.Controls.Add(btn21);
+            flowLayoutPanel1.Controls.Add(btn22);
+            flowLayoutPanel1.Controls.Add(btn23);
+            flowLayoutPanel1.Controls.Add(btn24);
+            flowLayoutPanel1.Controls.Add(btn25);
+            flowLayoutPanel1.Location = new Point(96, 191);
+            flowLayoutPanel1.Name = "flowLayoutPanel1";
+            flowLayoutPanel1.Size = new Size(957, 740);
+            flowLayoutPanel1.TabIndex = 0;
+            // 
+            // btn1
+            // 
+            btn1.Location = new Point(3, 3);
+            btn1.Name = "btn1";
+            btn1.Size = new Size(185, 142);
+            btn1.TabIndex = 0;
+            btn1.UseVisualStyleBackColor = true;
+            // 
+            // btn2
+            // 
+            btn2.Location = new Point(194, 3);
+            btn2.Name = "btn2";
+            btn2.Size = new Size(185, 142);
+            btn2.TabIndex = 1;
+            btn2.UseVisualStyleBackColor = true;
+            // 
+            // btn3
+            // 
+            btn3.Location = new Point(385, 3);
+            btn3.Name = "btn3";
+            btn3.Size = new Size(185, 142);
+            btn3.TabIndex = 2;
+            btn3.UseVisualStyleBackColor = true;
+            // 
+            // btn4
+            // 
+            btn4.Location = new Point(576, 3);
+            btn4.Name = "btn4";
+            btn4.Size = new Size(185, 142);
+            btn4.TabIndex = 3;
+            btn4.UseVisualStyleBackColor = true;
+            // 
+            // btn5
+            // 
+            btn5.Location = new Point(767, 3);
+            btn5.Name = "btn5";
+            btn5.Size = new Size(185, 142);
+            btn5.TabIndex = 4;
+            btn5.UseVisualStyleBackColor = true;
+            // 
+            // btn6
+            // 
+            btn6.Location = new Point(3, 151);
+            btn6.Name = "btn6";
+            btn6.Size = new Size(185, 142);
+            btn6.TabIndex = 5;
+            btn6.UseVisualStyleBackColor = true;
+            // 
+            // btn7
+            // 
+            btn7.Location = new Point(194, 151);
+            btn7.Name = "btn7";
+            btn7.Size = new Size(185, 142);
+            btn7.TabIndex = 6;
+            btn7.UseVisualStyleBackColor = true;
+            // 
+            // btn8
+            // 
+            btn8.Location = new Point(385, 151);
+            btn8.Name = "btn8";
+            btn8.Size = new Size(185, 142);
+            btn8.TabIndex = 7;
+            btn8.UseVisualStyleBackColor = true;
+            // 
+            // btn9
+            // 
+            btn9.Location = new Point(576, 151);
+            btn9.Name = "btn9";
+            btn9.Size = new Size(185, 142);
+            btn9.TabIndex = 8;
+            btn9.UseVisualStyleBackColor = true;
+            // 
+            // btn10
+            // 
+            btn10.Location = new Point(767, 151);
+            btn10.Name = "btn10";
+            btn10.Size = new Size(185, 142);
+            btn10.TabIndex = 9;
+            btn10.UseVisualStyleBackColor = true;
+            // 
+            // btn11
+            // 
+            btn11.Location = new Point(3, 299);
+            btn11.Name = "btn11";
+            btn11.Size = new Size(185, 142);
+            btn11.TabIndex = 10;
+            btn11.UseVisualStyleBackColor = true;
+            // 
+            // btn12
+            // 
+            btn12.Location = new Point(194, 299);
+            btn12.Name = "btn12";
+            btn12.Size = new Size(185, 142);
+            btn12.TabIndex = 11;
+            btn12.UseVisualStyleBackColor = true;
+            // 
+            // btn13
+            // 
+            btn13.Location = new Point(385, 299);
+            btn13.Name = "btn13";
+            btn13.Size = new Size(185, 142);
+            btn13.TabIndex = 12;
+            btn13.UseVisualStyleBackColor = true;
+            // 
+            // btn14
+            // 
+            btn14.Location = new Point(576, 299);
+            btn14.Name = "btn14";
+            btn14.Size = new Size(185, 142);
+            btn14.TabIndex = 13;
+            btn14.UseVisualStyleBackColor = true;
+            // 
+            // btn15
+            // 
+            btn15.Location = new Point(767, 299);
+            btn15.Name = "btn15";
+            btn15.Size = new Size(185, 142);
+            btn15.TabIndex = 14;
+            btn15.UseVisualStyleBackColor = true;
+            // 
+            // btn16
+            // 
+            btn16.Location = new Point(3, 447);
+            btn16.Name = "btn16";
+            btn16.Size = new Size(185, 142);
+            btn16.TabIndex = 15;
+            btn16.UseVisualStyleBackColor = true;
+            // 
+            // btn17
+            // 
+            btn17.Location = new Point(194, 447);
+            btn17.Name = "btn17";
+            btn17.Size = new Size(185, 142);
+            btn17.TabIndex = 16;
+            btn17.UseVisualStyleBackColor = true;
+            // 
+            // btn18
+            // 
+            btn18.Location = new Point(385, 447);
+            btn18.Name = "btn18";
+            btn18.Size = new Size(185, 142);
+            btn18.TabIndex = 17;
+            btn18.UseVisualStyleBackColor = true;
+            // 
+            // btn19
+            // 
+            btn19.Location = new Point(576, 447);
+            btn19.Name = "btn19";
+            btn19.Size = new Size(185, 142);
+            btn19.TabIndex = 18;
+            btn19.UseVisualStyleBackColor = true;
+            // 
+            // btn20
+            // 
+            btn20.Location = new Point(767, 447);
+            btn20.Name = "btn20";
+            btn20.Size = new Size(185, 142);
+            btn20.TabIndex = 19;
+            btn20.UseVisualStyleBackColor = true;
+            // 
+            // btn21
+            // 
+            btn21.Location = new Point(3, 595);
+            btn21.Name = "btn21";
+            btn21.Size = new Size(185, 142);
+            btn21.TabIndex = 20;
+            btn21.UseVisualStyleBackColor = true;
+            // 
+            // btn22
+            // 
+            btn22.Location = new Point(194, 595);
+            btn22.Name = "btn22";
+            btn22.Size = new Size(185, 142);
+            btn22.TabIndex = 21;
+            btn22.UseVisualStyleBackColor = true;
+            // 
+            // btn23
+            // 
+            btn23.Location = new Point(385, 595);
+            btn23.Name = "btn23";
+            btn23.Size = new Size(185, 142);
+            btn23.TabIndex = 22;
+            btn23.UseVisualStyleBackColor = true;
+            // 
+            // btn24
+            // 
+            btn24.Location = new Point(576, 595);
+            btn24.Name = "btn24";
+            btn24.Size = new Size(185, 142);
+            btn24.TabIndex = 23;
+            btn24.UseVisualStyleBackColor = true;
+            // 
+            // btn25
+            // 
+            btn25.Location = new Point(767, 595);
+            btn25.Name = "btn25";
+            btn25.Size = new Size(185, 142);
+            btn25.TabIndex = 24;
+            btn25.UseVisualStyleBackColor = true;
+            // 
+            // btnPlay
+            // 
+            btnPlay.BackgroundImage = Properties.Resources.init;
+            btnPlay.BackgroundImageLayout = ImageLayout.Stretch;
+            btnPlay.Location = new Point(459, 76);
+            btnPlay.Name = "btnPlay";
+            btnPlay.Size = new Size(113, 99);
+            btnPlay.TabIndex = 1;
+            btnPlay.UseVisualStyleBackColor = true;
+            // 
+            // btnReiniciar
+            // 
+            btnReiniciar.BackgroundImage = Properties.Resources.reiniciar;
+            btnReiniciar.BackgroundImageLayout = ImageLayout.Stretch;
+            btnReiniciar.Location = new Point(616, 76);
+            btnReiniciar.Name = "btnReiniciar";
+            btnReiniciar.Size = new Size(113, 99);
+            btnReiniciar.TabIndex = 2;
+            btnReiniciar.UseVisualStyleBackColor = true;
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.FromArgb(140, 55, 38);
+            panel1.Dock = DockStyle.Top;
+            panel1.Location = new Point(0, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(1158, 70);
+            panel1.TabIndex = 4;
             // 
             // UIAlgoritmo
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
-            Controls.Add(lbResponse);
+            ClientSize = new Size(1158, 964);
+            Controls.Add(panel1);
+            Controls.Add(btnReiniciar);
+            Controls.Add(btnPlay);
+            Controls.Add(flowLayoutPanel1);
+            FormBorderStyle = FormBorderStyle.None;
+            Margin = new Padding(4, 5, 4, 5);
             Name = "UIAlgoritmo";
             Text = "Form1";
+            
+            flowLayoutPanel1.ResumeLayout(false);
             ResumeLayout(false);
-            PerformLayout();
         }
 
         #endregion
 
-        private Label lbResponse;
+        private FlowLayoutPanel flowLayoutPanel1;
+        private Button btn1;
+        private Button btn2;
+        private Button btn3;
+        private Button btn4;
+        private Button btn5;
+        private Button btn6;
+        private Button btn7;
+        private Button btn8;
+        private Button btn9;
+        private Button btn10;
+        private Button btn11;
+        private Button btn12;
+        private Button btn13;
+        private Button btn14;
+        private Button btn15;
+        private Button btn16;
+        private Button btn17;
+        private Button btn18;
+        private Button btn19;
+        private Button btn20;
+        private Button btn21;
+        private Button btn22;
+        private Button btn23;
+        private Button btn24;
+        private Button btn25;
+        private Button btnPlay;
+        private Button btnReiniciar;
+        private Panel panel1;
     }
 }
--- UIAlgoritmo.cs
+++ UIAlgoritmo.cs
@@ -1,11 +1,127 @@
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Collections.Generic;
+
 namespace A_EstrellaV2
 {
     public partial class UIAlgoritmo : Form
     {
+        private int ordenClic = 0;
+
         public UIAlgoritmo()
         {
-            AEstrella.ForAtoM();
             InitializeComponent();
+            ConfigurarTablero();
+
+            if (btnReiniciar != null) btnReiniciar.Click += btnReiniciar_Click;
+            if (btnPlay != null) btnPlay.Click += btnInicio_Click;
+        }
+
+        private void ConfigurarTablero()
+        {
+            for (int i = 1; i <= 25; i++)
+            {
+                Control[] controles = this.Controls.Find("btn" + i, true);
+                if (controles.Length > 0 && controles[0] is Button btn)
+                {
+                    btn.BackgroundImage = null;
+                    btn.BackColor = Color.White;
+                    btn.Tag = "Vacio";
+                    btn.BackgroundImageLayout = ImageLayout.Stretch;
+                    btn.Click += BotonTablero_Click;
+                }
+            }
+        }
+
+        private void BotonTablero_Click(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            if (ordenClic == 0)
+            {
+                btn.BackgroundImage = Properties.Resources.inicio;
+                btn.Tag = "Inicio";
+                ordenClic = 1;
+            }
+            else if (ordenClic == 1)
+            {
+                if (btn.Tag.ToString() == "Inicio") return;
+                btn.BackgroundImage = Properties.Resources.fin;
+                btn.Tag = "Fin";
+                ordenClic = 2;
+            }
+            else
+            {
+                if (btn.Tag.ToString() == "Inicio" || btn.Tag.ToString() == "Fin") return;
+                if (btn.Tag.ToString() == "Vacio")
+                {
+                    btn.BackgroundImage = Properties.Resources.bombas;
+                    btn.Tag = "Bomba";
+                }
+                else
+                {
+                    btn.BackgroundImage = null;
+                    btn.Tag = "Vacio";
+                }
+            }
+        }
+
+        private void btnInicio_Click(object sender, EventArgs e)
+        {
+            
+            AEstrella.ForAtoM();
+
+            
+
+            int xActual = 0, yActual = 0, xMeta = 0, yMeta = 0;
+
+            for (int i = 1; i <= 25; i++)
+            {
+                Control[] c = this.Controls.Find("btn" + i, true);
+                if (c.Length > 0 && c[0] is Button btn)
+                {
+                    if (btn.Tag.ToString() == "Inicio") { xActual = (i - 1) % 5; yActual = (i - 1) / 5; }
+                    if (btn.Tag.ToString() == "Fin") { xMeta = (i - 1) % 5; yMeta = (i - 1) / 5; }
+                    if (btn.BackColor == Color.Green) btn.BackColor = Color.White;
+                }
+            }
+
+           //* RECORRIDO
+            PintarRecorrido(xActual, yActual, xMeta, yMeta);
+        }
+
+        private void PintarRecorrido(int xA, int yA, int xM, int yM)
+        {
+            int x = xA;
+            int y = yA;
+
+            
+            if (yA < yM) { while (y < yM) { AplicarColor(x, y); y++; } }
+            else { while (y > yM) { AplicarColor(x, y); y--; } }
+
+            // Luego en X
+            if (xA < xM) { while (x <= xM) { AplicarColor(x, yM); x++; } }
+            else { while (x >= xM) { AplicarColor(x, yM); x--; } }
+        }
+
+        private void AplicarColor(int x, int y)
+        {
+            int id = (y * 5) + x + 1;
+            Control[] c = this.Controls.Find("btn" + id, true);
+            if (c.Length > 0 && c[0] is Button btn)
+            {
+                // Solo pintamos si NO hay una bomba
+                if (btn.Tag.ToString() != "Bomba" && btn.Tag.ToString() == "Vacio")
+                {
+                    btn.BackColor = Color.Green;
+                }
+            }
+        }
+
+        private void btnReiniciar_Click(object sender, EventArgs e)
+        {
+            ordenClic = 0;
+            ConfigurarTablero();
         }
     }
 }
