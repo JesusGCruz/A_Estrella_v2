32ff242afdc70d654cc72232291c1df65daa5bae
jesus <jgarridocruz5@gmail.com>
2026-02-28 19:36:14 -0600

    Se modifico la funcion del algoritmo, se cambiaron los for por while para controlar el flujo, ahora el recorrido puede ser en reversa

--- UIAlgoritmo.cs
+++ UIAlgoritmo.cs
@@ -6,24 +6,60 @@ namespace A_EstrellaV2
         {
             InitializeComponent();
             // Coordenadas X, Y iniciales (a - Actual) y finales (m - Meta)
-            int xA, xM, yA, yM; 
-            xA = yA = 0;
-            xM = yM = 4;
-            
+            int xA, xM, yA, yM;
+            xA = yA = 4;
+            xM = yM = 0;
+
             lbResponse.Text = ForAtoM([xA, yA], [xM, yM]);
         }
         private string ForAtoM(int[] actual, int[] meta)
         {
             string res = "";
-            for (int y = actual[1]; y < meta[1]; y++)
-            {
-                res += actual[0] + " " + y + "\n";
+            int x = actual[0];
+            int y = actual[1];
+            if (actual[1] < meta[1])
+            {// Si Yactual es menor que Ymeta; se sigue con normalidad
+                while (y < meta[1])
+                {
+                    res += actual[0] + " " + y + "\n";
+                    y++;
+                }
             }
-            for (int x = actual[0]; x <= meta[0]; x++)
-            {
-                res += x + " " + meta[1] + "\n";
+            else
+            {// Si Yactual es mayor que Ymeta; se va pa atrás
+                while (y > meta[1])
+                {
+                    res += actual[0] + " " + y + "\n";
+                    y--;
+                }
             }
-            return res;
+            if (actual[0] < meta[0])
+            {// Lo mismo de arriba pero con X
+                while (x <= meta[0])
+                {
+                    res += x + " " + meta[1] + "\n";
+                    x++;
+                }
+            }
+            else
+            {
+                while (x >= meta[0])
+                {
+                    res += x + " " + meta[1] + "\n";
+                    x--;
+                }
+            }
+                /*
+                for (int y = actual[1]; y < meta[1]; y++)
+                {
+                    res += actual[0] + " " + y + "\n";
+                }
+                for (int x = actual[0]; x <= meta[0]; x++)
+                {
+                    res += x + " " + meta[1] + "\n";
+                }
+                 */
+                return res;
         }
     }
 }
